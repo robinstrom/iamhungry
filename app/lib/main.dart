f7a0ba2a556380ef15521edb7c4bd79393dbbe9f
robinstrom <robinstrom94@gmail.com>
2019-11-06 23:56:31 +0100

    make appbar transparent and in correct order and fill background image on to the whole screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,98 +1,75 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   static const String _title = 'iamhungry';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: _title,
-      home: Stack(children: <Widget>[
-        Scaffold(
-            backgroundColor: Colors.transparent,
-            appBar: AppBar(
-                title: const Text('iamhungry'),
-                elevation: 0.0,
-                actions: <Widget>[
-                  IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-                  IconButton(icon: Icon(Icons.navigate_next), onPressed: () {}),
-                ]),
-            body: Container(
-              color: Colors.transparent,
-            )),
-        Container(child: MyStatefulWidget())
-      ]),
+      theme: new ThemeData(),
+      home: new MyStatefulWidget(),
+      builder: (BuildContext context, Widget child) {
+        return Padding(
+          child: child,
+          padding: EdgeInsets.only(bottom: 50.0),
+        );
+      },
     );
   }
 }
 
 class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
   @override
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  void initState() {
-    super.initState();
-  }
-
-  void dispose() {
-    super.dispose();
-  }
-
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text('iamhungry'),
-          backgroundColor: Colors.black87,
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-            IconButton(icon: Icon(Icons.navigate_next), onPressed: () {}),
-          ]),
-      body: Stack(children: <Widget>[
-        Opacity(
-          opacity: 0.7,
-          child: Container(
-              decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('images/avocado-egg-sandwich.png'),
+    return Stack(
+      children: <Widget>[
+        new Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+                image: new AssetImage('images/avocado-egg-sandwich.png'),
                 fit: BoxFit.cover),
-          )),
-        ),
-        Container(
-          alignment: Alignment.center,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(30.0),
-                width: 350,
-                child: TextField(
-                  style: TextStyle(color: Colors.white, fontSize: 30),
-                  decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.all(10.0),
-                      filled: true,
-                      fillColor: Colors.black87,
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              Container(
-                child: Text(
-                  'Here should the Icon be',
-                ),
-              )
-            ],
           ),
         ),
-      ]),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: new AppBar(
+            leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+            title: new Text('iamhungry'),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            actions: <Widget>[
+              IconButton(icon: Icon(Icons.navigate_next), onPressed: () {}),
+            ],
+          ),
+          body: new Container(
+            alignment: Alignment.center,
+            child: new TextField(
+              style: new TextStyle(color: Colors.white, fontSize: 30),
+              decoration: new InputDecoration(
+                  contentPadding: new EdgeInsets.all(10.0),
+                  filled: true,
+                  fillColor: Colors.black87,
+                  prefixIcon: new Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  border: new OutlineInputBorder()),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
